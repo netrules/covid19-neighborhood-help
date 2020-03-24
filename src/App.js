b14efc1f18b66809cb81ed44c48be8b246b69290
Brandon Brule <bbrule@cbnco.com>
2020-03-24 18:07:54 -0400

    Added request path

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/about">About</Link>
+              <Link to="/about">Request</Link>
             </li>
             <li>
               <Link to="/users">Users</Link>
