ff9387446b951024c985d1c29ecb6c2355e30e8d
Brandon Brule <bbrule@cbnco.com>
2020-03-24 17:53:17 -0400

    Updates

---
+++ precache-manifest.3d4114579ee93531f126316cf946ce6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171b5cc3a8c6a7d355c2aee499c0b970",
+    "url": "/covid19-neighborhood-help/index.html"
+  },
+  {
+    "revision": "266d51498b1c89fda1da",
+    "url": "/covid19-neighborhood-help/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0def210263e90632cd6e",
+    "url": "/covid19-neighborhood-help/static/js/2.8d9d39d3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19-neighborhood-help/static/js/2.8d9d39d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266d51498b1c89fda1da",
+    "url": "/covid19-neighborhood-help/static/js/main.b59c9d84.chunk.js"
+  },
+  {
+    "revision": "892dfa2de53a2c7d885e",
+    "url": "/covid19-neighborhood-help/static/js/runtime-main.ab59e0f3.js"
+  }
+]);
