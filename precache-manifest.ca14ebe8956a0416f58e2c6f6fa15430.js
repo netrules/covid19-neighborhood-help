54a1278c2967879b9a0a5debcd5685e81729cb70
Eli Edds <codekrafter@gmail.com>
2020-03-24 20:10:57 -0400

    Updates

---
+++ precache-manifest.ca14ebe8956a0416f58e2c6f6fa15430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f71c68a9ba1bcf04e16c6ea54c72c3",
+    "url": "/covid19-neighborhood-help/index.html"
+  },
+  {
+    "revision": "4a59fa45f4064ec77c4b",
+    "url": "/covid19-neighborhood-help/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "0b74790d94c40043673d",
+    "url": "/covid19-neighborhood-help/static/js/2.9b4a3ece.chunk.js"
+  },
+  {
+    "revision": "088762ca49c61ec7ee3934bedd4dacda",
+    "url": "/covid19-neighborhood-help/static/js/2.9b4a3ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a59fa45f4064ec77c4b",
+    "url": "/covid19-neighborhood-help/static/js/main.0b66118a.chunk.js"
+  },
+  {
+    "revision": "892dfa2de53a2c7d885e",
+    "url": "/covid19-neighborhood-help/static/js/runtime-main.ab59e0f3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-neighborhood-help/precache-manifest.f2ff5f845421a5d8ef65bdd8f7ea7f7d.js"
+  "/covid19-neighborhood-help/precache-manifest.ca14ebe8956a0416f58e2c6f6fa15430.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19-neighborhood-help/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
