b5d800a04bd51177174e94091ed26922ef3ca4ff
Santiago González <santiago.publicmail@gmail.com>
2020-03-27 23:46:48 +0000

    Adding provisional tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders request link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/request/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders volunteer link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/volunteer/i);
   expect(linkElement).toBeInTheDocument();
 });
