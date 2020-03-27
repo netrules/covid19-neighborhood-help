00339123fe8ca40f5a520ffcf757a37653db4a04
Brandon Brule <brandonbrule@gmail.com>
2020-03-26 20:53:56 -0400

    Update requester-page.js
    Form Submits Entries to Firebase.

--- src/components/requester/requester-page.js
+++ src/components/requester/requester-page.js
@@ -14,11 +14,91 @@ import {
 
 import {Carousel} from '../carousel';
 
+import * as firebase from 'firebase';
+
+
+
+/*
+email
+
+location_description
+
+location_geo
+
+message
+
+phone
+
+time_created
+
+username
+
+
+*/
+
+
 // Volunteer Page
 export class RequesterPage extends React.Component {
-    render() {
-        return <Request />;
-    }
+  constructor () {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      location_description: '',
+      message: '',
+      phone: '',
+      time_created: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange (evt) {
+    // check it out: we get the evt.target.name (which will be either "email" or "password")
+    // and use it to target the key on our `state` object with the same name, using bracket syntax
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  handleSubmit(event) {
+    let day = firebase.firestore.Timestamp.fromDate(new Date());
+    this.state.time_created = day;
+    firebase.firestore().collection("requests").add(this.state);
+    event.preventDefault();
+  }
+  
+  render () {
+    return (
+      <form onSubmit={this.handleSubmit}>
+
+        <fieldset>
+          <label>Name</label>
+          <input type="text" name="name" onChange={this.handleChange} />
+        </fieldset>
+
+        <fieldset>
+          <label>Email</label>
+          <input type="text" name="email" onChange={this.handleChange} />
+        </fieldset>
+
+        <fieldset>
+          <label>Location Description</label>
+          <input type="text" name="location_description" onChange={this.handleChange} />
+        </fieldset>
+
+        <fieldset>
+          <label>Message</label>
+          <input type="text" name="message" onChange={this.handleChange} />
+        </fieldset>
+        
+        <fieldset>
+          <label>Phone</label>
+          <input name="phone" onChange={this.handleChange} />
+        </fieldset>
+        
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
 
@@ -47,4 +127,7 @@ function Request() {
       </div>
     );
   }
+
+
+
 
